8fa2ffab3fff2f763d5d469071c32242ffd23bb0
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-12-29 15:42:36 -0500

    add ngx-plyr and basic usage of it against local streammachine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PlyrComponent } from 'ngx-plyr';
+import * as Plyr from 'plyr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'npr2';
+
+  player: Plyr;
+
+  audioSources = [
+    {
+      src: 'http://localhost:8001',
+      type: 'audio/mp3',
+    }
+  ];
+
+  played(event: Plyr.PlyrEvent) {
+    console.log('played', event);
+    console.log(this.audioSources);
+
+  }
+
+  playLive(): void {
+    this.audioSources = [
+      {
+        src: 'http://localhost:8001',
+        type: 'audio/mp3',
+      }
+    ];
+  }
+
+  playTop(): void {
+    var offset = new Date().getMinutes()*60 + new Date().getSeconds();
+    this.audioSources = [
+      {
+        src: 'http://localhost:8001/test?offset='+offset,
+        type: 'audio/mp3',
+      }
+    ];
+  }
+
+  ready(): void {
+    this.player.play();
+  }
+
 }
